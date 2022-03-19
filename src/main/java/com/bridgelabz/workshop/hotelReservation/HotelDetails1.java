7b502e4cf45d3754d61fe4deda09601479012c57
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-03-19 23:16:56 +0530

    Added UC-2 Problem

--- src/main/java/com/bridgelabz/workshop/hotelReservation/HotelDetails1.java
+++ src/main/java/com/bridgelabz/workshop/hotelReservation/HotelDetails1.java
@@ -1,25 +1,37 @@
 package com.bridgelabz.workshop.hotelReservation;
 
-public class HotelDetails {
+public class HotelDetails1 {
 	private String name;
 	private int price;
-	public HotelDetails(String name, int price) {
+
+	/**
+	 * 1. Created a constructor
+	 * 
+	 * @param name
+	 * @param price
+	 */
+	public HotelDetails1(String name, int price) {
 		super();
 		this.name = name;
 		this.price = price;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	@Override
 	public String toString() {
 		return "\nHotelDetails [name=" + name + ", price=" + price + "]";
