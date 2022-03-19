0005da9c24db1dc77fb2b27d97bed248355d7440
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-03-19 11:39:26 +0530

    Added UC-1 Problem

---
+++ src/main/java/com/bridgelabz/workshop/hotelReservation/HotelDetails.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.workshop.hotelReservation;
+
+public class HotelDetails {
+	private String name;
+	private int price;
+	public HotelDetails(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "\nHotelDetails [name=" + name + ", price=" + price + "]";
+	}
+
+}
